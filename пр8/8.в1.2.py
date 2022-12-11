a693ac6d38225f19d309746ada9f9bf1b713b3c0
Mok-hub87 <113777664+Mok-hub87@users.noreply.github.com>
2022-12-11 21:25:02 +0300

    Add files via upload

--- пр8/8.в1.2.py
+++ пр8/8.в1.2.py
@@ -1,19 +1,16 @@
-k=int(input())
-a=list(map(int,input().split()))[:k]
-b=list(map(int,input().split()))[:k]
-c=list(map(int,input().split()))[:k]
-suma=sum(a)
-sumb=sum(b)
-sumc=sum(c)
-la=len(a)
-lb=len(b)
-lc=len(c)
-mida=suma/la
-midb=sumb/lb
-midc=sumc/lc
-print(suma,mida)
-print(sumb,midb)
-print(sumc,midc)
-print(a)
-print(b)
-print(c)
+def F(a):
+    return [i for i in range (a,16)]
+def F1(b):
+    return [i for i in range (b,16)]
+def F2(c):
+    return [i for i in range (c,16)]
+s1=sum(F(1))
+l1=len(F(1))
+m1=s1/l1
+s2=sum(F1(2))
+l2=len(F1(2))
+m2=s2/l2
+s3=sum(F(3))
+l3=len(F(3))
+m3=s3/l3
+print('1)',s1,m1,'2)',s2,m2,'3)',s3,m3)
